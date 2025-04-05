f5a69f6ac2ac5df28bb0aa19a5b10f114874a4a0
MasterOktagon <userkontakt.hexagongames@gmail.com>
2025-04-05 14:28:23 +0200

    commit

--- terrain.gd
+++ terrain.gd
@@ -4,7 +4,10 @@ extends Sprite2D
 
 
 func _process(delta: float) -> void:
-	scale = get_viewport_rect().size
+	scale = get_viewport_rect().size * 1.1
+	#scale = scale / $"../Player/MainCamera".zoom
 	position = $"../Player".position
 	material.set_shader_parameter("offset", position)
 	material.set_shader_parameter("size", scale)
+	material.set_shader_parameter("zoom", $"../Player/MainCamera".zoom)
+	
