46a36e26947f24efcdd3e4aeb2e868689efeb2d1
MasterOktagon <userkontakt.hexagongames@gmail.com>
2025-04-05 12:22:01 +0200

    CHANGE [WIP]: better terrain shader

--- scripts/map.gd
+++ scripts/map.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-var heightmap : PackedColorArray = []#PackedColorArray(preload("res://assets/heightmap/VirginLands.png").get_image().get_data())
+var heightmap : PackedByteArray = preload("res://assets/heightmap/VirginLands.png").get_image().get_data()
 
 func _ready() -> void:
-	pass
+	print(heightmap.size())
+	print(4096*2048*3)
+	print(get_height(1024, 2048))
 	
 	
 func get_height(x: int, y: int) -> int:
-	return heightmap[y*4096+x].r-150
+	return heightmap[(y*4096+x)*3]-150
