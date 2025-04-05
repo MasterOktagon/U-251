841e1b97774c96800b6bba44f81eb2dc5df36589
MasterOktagon <userkontakt.hexagongames@gmail.com>
2025-04-05 13:04:18 +0200

    ADD: water shader (again)

--- terrain.gd
+++ terrain.gd
@@ -1,6 +1,10 @@
+@tool
 extends Sprite2D
+
 
 
 func _process(delta: float) -> void:
 	scale = get_viewport_rect().size
 	position = $"../Player".position
+	material.set_shader_parameter("offset", position)
+	material.set_shader_parameter("size", scale)
