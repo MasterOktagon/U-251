d36394bee0b6728bb784573917c25eb1913b8016
MasterOktagon <userkontakt.hexagongames@gmail.com>
2025-04-05 11:09:24 +0200

    ADD: get_height function of Map

---
+++ scripts/map.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var heightmap : PackedColorArray = PackedColorArray(preload("res://assets/heightmap/VirginLands.png").get_image().get_data())
+
+func _ready() -> void:
+	pass
+	
+	
+func get_height(x: int, y: int) -> int:
+	return heightmap[y*4096+x].r-150
