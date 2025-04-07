6c6eac00a37fa0c5b16f81318164bea4fc3d1acf
unknown <nizdvfc@web.de>
2025-04-07 13:01:46 +0200

    ADD: checkpoints

--- scripts/level.gd
+++ scripts/level.gd
@@ -10,6 +10,7 @@ var map_scale: int = 0
 var delete_radius: float = 2000
 var start_pos := Vector2(0,0)
 var start_rot: float = 0
+var checkpoints: Array[Vector2] = []
 
 func load_VirginLands():
 	height_min = -255
@@ -18,5 +19,6 @@ func load_VirginLands():
 	heightmap = preload("res://assets/heightmap/VirginLands.png").get_image().get_data()
 	map_size = Vector2i(4096, 4096)
 	map_scale = 10
-	start_pos = Vector2(500,500)*map_scale
+	start_pos = Vector2(500,500)
 	start_rot = PI
+	checkpoints = [Vector2(100,100), Vector2(200,200), Vector2(350,100)]
